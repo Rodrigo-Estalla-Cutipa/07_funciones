590a5f30670d2a49cd9b90f8fc6e39368d8c7b76
Rodrigo-Estalla-Cutipa <restallac@unjbg.edu.pe>
2025-05-29 15:34:10 -0500

    Actualiza ejercicio 3: Agrega funciones

--- Ejercicio_3.cpp
+++ Ejercicio_3.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
 using namespace std;
 
+void operacion(int);
+void suma(int);
+
 int main(){
-	int a, b, n, t, suma, i;
-	a = 0;
-	b = 1;
-    suma = 0;
+	int n;
 	
 	cout << "Ingrese el valor de n" << endl;
 	cin >> n;
 	cout << "Los primeros n terminos de la serie Fibonacci son:" << endl;
 	
+	operacion(n);
+
+	return 0;
+}
+
+void operacion(int n){
+	int a, b, t, s, i;
+	a = 0;
+	b = 1;
+	s = 0;
+
 	for (i=1; i<=n; i++) {
 		t = a + b;
 		cout << t << " ";
 		a = b;
 		b = t;
-        suma = suma + t;
+        s = s + t;
 	}
-	
-    cout << endl << "La suma de los terminos generados es: " << suma;
-	return 0;
+	suma(s);
+}
+
+void suma(int s){
+	cout << endl << "La suma de los terminos generados es: " << s;
 }
