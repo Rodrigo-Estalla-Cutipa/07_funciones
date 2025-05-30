3d1b50079d31e8d6d3595215ebaab62a4b6c649a
Rodrigo-Estalla-Cutipa <restallac@unjbg.edu.pe>
2025-05-30 18:31:06 -0500

    Actualiza ejercicio 4: Agrega una funcion al algoritmo

--- Ejercicio_4.cpp
+++ Ejercicio_4.cpp
@@ -1,25 +1,34 @@
 #include <iostream>
-
 using namespace std;
 
+int operacion(int);
+
 int main(){
-    int n, s, i;
-    s = 0;
+    int n, r;
 
     cout << "Ingrese un numero" << endl;
     cin >> n;
     
-    for (i=1; i<n; i++){
-        if (n % i == 0) {
-            s = s + i;
-        }
-    }
-    
-    if (s == n){
+    r = operacion(n);
+
+    if (r == n){
         cout << "El numero es perfecto";
     } else {
         cout << "El numero no es perfecto";
     }
 
     return 0;
+}
+
+int operacion (int n){
+    int s, i;
+    s = 0;
+    
+    for (i=1; i<n; i++){
+        if (n % i == 0) {
+            s = s + i;
+        }
+    }
+
+    return s;
 }
