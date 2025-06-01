449ad73cf63130ea34bb5a0e7d23959c252c6363
Rodrigo-Estalla-Cutipa <restallac@unjbg.edu.pe>
2025-06-01 18:13:09 -0500

    Actualiza ejercicio 6: Agrega funcion al algoritmo

--- Ejercicio_6.cpp
+++ Ejercicio_6.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+void calculo (int);
+
 int main(){
     int n, r, i;
     r = 0;
@@ -13,17 +15,23 @@ int main(){
         cin >> n;
     }
 
+    calculo (n);
+
+    return 0;
+}
+
+void calculo(int n){
+    int r, i;
+    r = 0;
+
     for (i=1; i<=n; i++) {
         if (n % i == 0) {
             r = r + 1;
         }
     }
-
     if (r==2) {
         cout << "El numero es primo";
     } else {
         cout << "El numero no es primo";
     }
-
-    return 0;
 }
